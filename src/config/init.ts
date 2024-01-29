2bb21c74b96d8fd0d357e6c3a97f2165d8b2b77c
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-01-29 14:47:28 +0500

    feat: initalize pingpong to start ponging

---
+++ src/config/init.ts
@@ -0,0 +1,26 @@
+import { Env_Vars } from "./env";
+import { PingPong } from "../controllers/pingPong/pingPong";
+import { EnvClassConstructorArgs } from "./types";
+
+const envArg: EnvClassConstructorArgs = {
+  alchemyEnv: {
+    ALCHEMY_KEY: process.env.ALCHEMY_KEY,
+  },
+  pingPongEnv: {
+    PONGER_KEY: process.env.PONGER_KEY,
+    PINGPONG_ADDRESS: process.env.PINGPONG_ADDRESS,
+    PINGPONG_STARTING_BLOCK: process.env.PINGPONG_STARTING_BLOCK,
+  },
+  appEnv: {
+    BACKEND_PORT: process.env.BACKEND_PORT,
+  },
+};
+
+export const env_Vars = new Env_Vars(envArg);
+
+export const pingPong = new PingPong(
+  env_Vars.Alchemy.ALCHEMY_KEY,
+  env_Vars.PingPong.PINGPONG_ADDRESS,
+  env_Vars.PingPong.PONGER_KEY,
+  env_Vars.PingPong.PINGPONG_STARTING_BLOCK
+);
