65839d7ffbd6edbe5249e2023b939d05128dbeb6
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-01-29 15:44:44 +0500

    feat: start app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello word");
+import express from "express";
+import { env_Vars } from "./config/init";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "PingPong Bot Up",
+    startingBlock: env_Vars.PingPong.PINGPONG_STARTING_BLOCK,
+  });
+});
+
+// railway provides this PORT directly
+const PORT = process.env.PORT || env_Vars.App.BACKEND_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT} - pingPongBot`);
+});
