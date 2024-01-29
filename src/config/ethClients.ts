ffa37a76082627ed7df8fa0cc76f486c5033233f
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-01-29 13:55:46 +0500

    feat: add ethereum clients

---
+++ src/config/ethClients.ts
@@ -0,0 +1,40 @@
+import { createPublicClient, createWalletClient, http } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia, goerli } from "viem/chains";
+
+const CHAINS = {
+  sepolia: {
+    viemChain: sepolia,
+    getHttpSocketUrl: (_alchemyKey: string) =>
+      http(`https://eth-sepolia.g.alchemy.com/v2/${_alchemyKey}`),
+  },
+  goerli: {
+    viemChain: goerli,
+    getHttpSocketUrl: (_alchemyKey: string) =>
+      http(`https://eth-sepolia.g.alchemy.com/v2/${_alchemyKey}`),
+  },
+} as const;
+
+const selectedChain = CHAINS.sepolia;
+
+export const getPublicClient = (_alchemyKey: string) =>
+  createPublicClient({
+    chain: selectedChain.viemChain,
+    transport: selectedChain.getHttpSocketUrl(_alchemyKey),
+    // batch: {
+    //   multicall: true,
+    // },
+  });
+
+const getPongerAccount = (_pongerPrivateKey: string) =>
+  privateKeyToAccount(`0x${_pongerPrivateKey}`);
+
+export const getPongerClient = (
+  _pongerPrivateKey: string,
+  _alchemyKey: string
+) =>
+  createWalletClient({
+    account: getPongerAccount(_pongerPrivateKey),
+    chain: selectedChain.viemChain,
+    transport: selectedChain.getHttpSocketUrl(_alchemyKey),
+  });
