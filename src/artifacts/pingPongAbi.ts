ccea6674461f6bef0f8df229dd9ff372df5e74c8
Muhammad Bin Aamir <81583119+wadader@users.noreply.github.com>
2024-01-29 13:28:09 +0500

    feat: add test abi

---
+++ src/artifacts/pingPongAbi.ts
@@ -0,0 +1,64 @@
+export const PINGPONG_ABI = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [],
+    name: "Ping",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "bytes32",
+        name: "txHash",
+        type: "bytes32",
+      },
+    ],
+    name: "Pong",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "ping",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pinger",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "_txHash",
+        type: "bytes32",
+      },
+      {
+        internalType: "bool",
+        name: "_shouldAccept",
+        type: "bool",
+      },
+    ],
+    name: "pong",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
